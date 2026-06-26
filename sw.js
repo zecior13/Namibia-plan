48334f9a49655415757b5be8ec3ecd57109c3ae9
zecior13 <mioduchowski.radomil@gmail.com>
2026-06-26 12:54:15 +0200

    Update service worker cache management

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "namibia-plan-v2";
+const CACHE_PREFIX = "namibia-plan-";
+const CACHE_NAME = "namibia-plan-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -7,20 +8,20 @@ const FILES_TO_CACHE = [
   "./data.js",
   "./app.js",
   "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/favicon-32.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return Promise.all(
+    caches.open(CACHE_NAME).then(cache =>
+      Promise.all(
         FILES_TO_CACHE.map(url =>
-          cache.add(url).catch(err => {
-            console.warn("Nie udało się dodać do cache:", url, err);
-          })
+          cache.add(url).catch(err => console.warn("Cache fail:", url, err))
         )
-      );
-    })
+      )
+    )
   );
   self.skipWaiting();
 });
@@ -30,7 +31,7 @@ self.addEventListener("activate", event => {
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) {
+          if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
